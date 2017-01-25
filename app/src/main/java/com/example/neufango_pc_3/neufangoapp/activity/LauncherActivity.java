8041e09246848babde412ac2b0efaafba181d2ec
Anamika <anamika.arya1990@gmail.com>
2017-01-25 16:27:25 +0530

    Adding logo in drawables

---
+++ app/src/main/java/com/example/neufango_pc_3/neufangoapp/activity/LauncherActivity.java
@@ -0,0 +1,14 @@
+package com.example.neufango_pc_3.neufangoapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.neufango_pc_3.neufangoapp.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
